274c08f5c2f24f8d2ddd4046a35d777a310b2aa5
luciapuccini <puccinilucia@gmail.com>
2020-04-10 22:03:33 -0300

    feat search

--- src/screens/Student.js
+++ src/screens/Student.js
@@ -1,18 +1,35 @@
 import React from 'react';
-import { ScrollView, View } from 'react-native';
-import { Item, Input, Icon } from 'native-base';
+import { TextInput, ScrollView, View } from 'react-native';
+import { Item, Icon, Input } from 'native-base';
 import { SubjectList } from './SubjectList';
 
 export const Student = () => {
+  const [subjects, setSubjects] = React.useState([]);
+  const [searchTerm, setSearchTerm] = React.useState('');
+
+  React.useEffect(() => {
+    fetch('http://www.mocky.io/v2/5e9108643300008c00e9cd5a')
+      .then((response) => response.json())
+      .then((json) => {
+        setSubjects(json);
+      });
+  }, [searchTerm]);
+
+  const results = !searchTerm
+    ? subjects
+    : subjects.filter((subject) =>
+        subject.name.toLowerCase().includes(searchTerm.toLocaleLowerCase()),
+      );
+
   return (
     <>
       <View style={{ marginLeft: 20, marginTop: 10, width: '90%' }}>
         <Item>
           <Icon active name="search" />
-          <Input placeholder="Materia" />
+          <Input placeholder="Materia" onChangeText={setSearchTerm} />
         </Item>
       </View>
-      <SubjectList />
+      <SubjectList subjects={results} />
     </>
   );
 };
--- src/screens/SubjectList.js
+++ src/screens/SubjectList.js
@@ -9,19 +9,20 @@ const findSubjectImage = (subjectName) => {
   return subjectPlaceholder;
 };
 
-export const SubjectList = () => {
-  const [subjects, setSubjects] = useState([]);
+export const SubjectList = ({ subjects }) => {
+  // const [subjects, setSubjects] = useState([]);
+
   useEffect(() => {
-    fetch('http://www.mocky.io/v2/5e9108643300008c00e9cd5a')
-      .then((response) => response.json())
-      .then((json) => {
-        setSubjects(json);
-      });
+    // fetch('http://www.mocky.io/v2/5e9108643300008c00e9cd5a')
+    //   .then((response) => response.json())
+    //   .then((json) => {
+    //     setSubjects(json);
+    //   });
   }, []);
 
   const renderItem = ({ item, index }) => {
     const img = findSubjectImage(item.name);
-    return <SubjectCard key={item.subjectId} name={item.name} image={img} />;
+    return <SubjectCard name={item.name} image={img} />;
   };
 
   return <FlatList data={subjects} renderItem={renderItem} />;
