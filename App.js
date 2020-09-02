86747c0f258b1d2f8378985cd976794abafefa8a
luciapuccini <puccinilucia@gmail.com>
2020-09-01 21:58:25 -0300

    WIP messaging

--- App.js
+++ App.js
@@ -4,16 +4,74 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import * as eva from '@eva-design/eva';
+import messaging from '@react-native-firebase/messaging';
 import _ from 'underscore';
 
 import { RootStack, AuthStack } from './src/Routes';
 import { Context, Provider } from './src/context/AuthContext';
 import { ThemeContext } from './/src/context/ThemeContext';
+import { getToken } from './src/utils/authHelper';
 const { Navigator, Screen } = createStackNavigator();
 
 const App = ({ navigation }) => {
   const { state, restore } = React.useContext(Context);
+  const [clase, setClase] = React.useState(null);
+  const [subject, setSubject] = React.useState(null);
+
   React.useEffect(() => {
+    // 1. handle app running in bgr
+    messaging().onNotificationOpenedApp(async (remoteMessage) => {
+      const { classId } = remoteMessage.data;
+      const fetchClassData = async () => {
+        const token = await getToken();
+        return fetch(
+          `http://181.164.121.14:25565/clases/findClassData/${classId}`,
+          {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: `Bearer ${token}`,
+            },
+          },
+        )
+          .then((response) => response.json())
+          .then((json) => {
+            if (json) {
+              setClase(json);
+              setSubject(json.subject);
+              return 'ok';
+            }
+          })
+          .catch((error) => {
+            console.log('[ FAILED ]', error);
+          });
+      };
+      const response = await fetchClassData();
+      console.log('App -> response', response, clase, subject);
+      if (response) {
+        console.log('algo existe', response, clase, subject);
+        navigation.navigate('Class Detail', {
+          clase,
+          manager: false,
+          subject,
+        });
+      }
+    });
+
+    // 2. handle app open from quiet state
+    messaging()
+      .getInitialNotification()
+      .then((remoteMessage) => {
+        //WARNING este caso esta null
+        console.log('remote', remoteMessage);
+        if (remoteMessage) {
+          console.log(
+            'Notification caused app to open from quit state:',
+            remoteMessage.notification,
+          );
+          // setInitialRoute(remoteMessage.data.type); // e.g. "Settings"
+        }
+      });
     restore();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
@@ -28,12 +86,12 @@ const App = ({ navigation }) => {
             options={{ headerShown: false }}
           />
         ) : (
-          <Screen
-            name="Routes"
-            component={RootStack}
-            options={{ headerShown: false }}
-          />
-        )}
+            <Screen
+              name="Routes"
+              component={RootStack}
+              options={{ headerShown: false }}
+            />
+          )}
       </Navigator>
     </NavigationContainer>
   );
